9e80ee002e8ea494e2508c739aaf7c69ed8b4074
Christophe Parisse <cparisse@parisnanterre.fr>
2020-07-09 10:13:39 +0200

    version 1.40.33

--- dist.sh
+++ dist.sh
@@ -1,4 +1,5 @@
 cp -v target/teicorpo-*-SNAPSHOT.jar teicorpo.jar
 scp teicorpo.jar parisse@ct3.ortolang.fr:/applis/download/
+scp teicorpo.jar parisse@ct3.ortolang.fr:/applis/tools/
 scp teicorpo.jar parisse@ct3.ortolang.fr:/applis/ct3/teicorpo-doc/
 scp teicorpo.jar parisse@ct3.ortolang.fr:/applis/teiconvert/system/
