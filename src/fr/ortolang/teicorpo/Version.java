fe304fc64e37b53ab5b957520ae09d977ebac5b3
Christophe Parisse <cparisse@parisnanterre.fr>
2025-10-23 21:43:39 +0200

    23 oct 202

--- src/fr/ortolang/teicorpo/Version.java
+++ src/fr/ortolang/teicorpo/Version.java
@@ -3,8 +3,8 @@ package fr.ortolang.teicorpo;
 public class Version {
 
 	public static String versionTEI = "0.9.1";
-	private static String versionSoftStr = "1.41.17"; // full version with Elan, Clan, Transcriber and Praat
-	private static String versionDateStr = "6/12/2024 12:00";
+	private static String versionSoftStr = "1.41.18"; // full version with Elan, Clan, Transcriber and Praat
+	private static String versionDateStr = "23/10/2025 12:00";
 
 	public static String versionSoft(boolean isTest) {
 		return isTest ? "testing-version" : versionSoftStr;
